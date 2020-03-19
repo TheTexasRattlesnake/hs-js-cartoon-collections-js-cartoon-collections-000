40f1caee5876c139376386527d93069a181153b5
Dawson Harris <dlharris1017@gmail.com>
2020-03-19 21:07:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var array = []
+  var num = 0
+  for (num = 0; num < dwarves.length; num++) {
+    array.push(`${num+1}. ${dwarves[num]}`)
+  }
+  return array.toString().replace(/,/g, ' ')
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+   var array = []
+  for (var i=0;i<planeteerCalls.length;i++) {
+    array.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+  return array
 }
 
 function longPlaneteerCalls(words) {
+  for (var i=0;i<words.length;i++) {
+    if (words[i].length > 4) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  var cheese = ['cheddar','gouda','camembert']
+  for (var i=0;i<foods.length;i++) {
+    if (cheese.indexOf(foods[i]) > -1) {
+      return foods[i];
+      }
+  }
+  return 'no cheese!'
 }
